964a20f45404e0f4bbd0192169acf657b29e52b1
Ravindu Sandeep <sandeepravindu112@gmail.com>
2025-07-09 00:16:41 +0530

    I added nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <main className="container p-4 mx-auto">
+         
+      </main>
     </div>
   )
 }
