469a6cd785061877a5773f52fcead2a4c8bc96eb
Ravindu Sandeep <sandeepravindu112@gmail.com>
2025-07-10 03:32:43 +0530

    I have updated my full project structure

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,82 @@
-import reactlogo from './react-logo.svg';
+import about_img from './about_img.png'
+import contact_img from './contact_img.png'
+import exchange_icon from './exchange_icon.png'
+import header_img from './header_img.png'
+import logo_footer from './logo-footer.png'
+import logo from './logo.png'
+import menu_icon from './menu_icon.png'
+import profile_icon from './profile-icon.png'
+import quality_icon from './quality_icon.png'
+import rectangle3608 from './Rectangle 3608.png'
+import rectangle3609 from './Rectangle 3609.png'
+import rectangle3611 from './Rectangle 3611.png'
+import rectangle3613_1 from './Rectangle 3613 (1).png'
+import rectangle3613 from './Rectangle 3613.png'
+import rectangle3615 from './Rectangle 3615.png'
+import rectangle3616 from './Rectangle 3616.png'
+import rectangle3617_1 from './Rectangle 3617 (1).png'
+import rectangle3617 from './Rectangle 3617.png'
+import rectangle3619 from './Rectangle 3619.png'
+import rectangle3621 from './Rectangle 3621.png'
+import rectangle3622 from './Rectangle 3622.png'
+import rectangle3626_1 from './Rectangle 3626 (1).png'
+import rectangle3626 from './Rectangle 3626.png'
+import rectangle3627_1 from './Rectangle 3627 (1).png'
+import rectangle3627 from './Rectangle 3627.png'
+import rectangle3628 from './Rectangle 3628.png'
 
 export const assets = {
-    reactlogo,
+  about_img,
+  contact_img,
+  exchange_icon,
+  header_img,
+  logo_footer,
+  logo,
+  menu_icon,
+  profile_icon,
+  quality_icon,
+  rectangle3608,
+  rectangle3609,
+  rectangle3611,
+  rectangle3613_1,
+  rectangle3613,
+  rectangle3615,
+  rectangle3616,
+  rectangle3617_1,
+  rectangle3617,
+  rectangle3619,
+  rectangle3621,
+  rectangle3622,
+  rectangle3626_1,
+  rectangle3626,
+  rectangle3627_1,
+  rectangle3627,
+  rectangle3628,
 }
 
+export const products = [
+  {
+    _id: "aaaaa",
+    name: "Women Round Neck Cotton Top",
+    description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline.",
+    price: 100,
+    image: [rectangle3608],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true
+  },
+  {
+    _id: "aaaab",
+    name: "Men Round Neck Pure Cotton T-shirt",
+    description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline.",
+    price: 200,
+    image: [rectangle3609,rectangle3611,rectangle3613_1,rectangle3613],
+    category: "Men",
+    subCategory: "Topwear",
+    sizes: ["M", "L", "XL"],
+    date: 1716621345448,
+    bestseller: true
+  }
+];
